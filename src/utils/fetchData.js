b783284dcef2197eaadbcbb6ac17b4802666c563
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-07-19 17:45:17 -0500

    working o theon videos

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -10,7 +10,7 @@ export const youtubeOptions = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
   }
 };
 
